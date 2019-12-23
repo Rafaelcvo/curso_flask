f05600633379c5d7a5bb618109b66338c7c5a8d6
not <rafaelmoreiracvo@gmail.com>
2019-12-23 11:13:27 -0300

    criando paginas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
 
-
+@app.route("/index/")
 @app.route("/")
 def index():
-	return "Hello Word 2"
+	return render_template('index.html')
+
+@app.route("/login")
+def login():
+	return render_template('base.html')
+
+
+
+	# @app.route("/base/<user>")
+	# @app.route("/base/", defaults={"user":None})
+	# def base(user):
+	# 	return render_template('base.html', user=user)
+
+	#
+	# @app.route("/test", defaults={'name':None})
+	# @app.route("/test/", defaults={'name':None})
+	# @app.route("/test/<name>")
+	# def test(name):
+	# 	if name:
+	# 		return "Olá %s!" % name
+	# 	else:
+	# 		return "Olá usuario!"
