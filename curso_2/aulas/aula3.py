a59424aea1c41bbb59daf13681199608261d915a
rafael <rafaelmoreiacvo@yahoo.com.br>
2019-12-24 14:26:36 -0300

    aula 4 finalizada

--- curso_2/aulas/aula3.py
+++ curso_2/aulas/aula3.py
@@ -1,8 +1,18 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
-#
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name="visitante"):
+    return "Ola {}".format(name)
+
+
+@app.route("/admin/")
+def admin():
+    return "<h1> Admin </h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
